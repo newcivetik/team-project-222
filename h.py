5113d1120e9e2aa27942a7712b363c859162a719
Рафаэль <lili116mail.ru@gmail.com>
2025-06-18 19:23:29 +0300

    Удаление ненужных файлов

--- h.py
+++
@@ -1,9 +0,0 @@
-import tkinter as tk
-
-root = tk.Tk()
-root.title("Тест")
-label = tk.Label(root, text="Работает!")
-label.pack()
-root.mainloop()
-
-print("Приложение завершило работу")  # Добавьте эту строку
